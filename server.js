f454033dddcb8afa726618afcd8d33d6e96439f8
wminaung <minwin243@gmail.com>
2023-02-03 19:34:50 +0630

    change port

--- server.js
+++ server.js
@@ -131,7 +131,7 @@ const server = http.createServer((req, res) => {
     return res.end();
   }
 });
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 server.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
